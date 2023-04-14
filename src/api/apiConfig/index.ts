4db2503ee8c47a0bc9d101a6a0af3f4efe96187f
NastyaZ23 <nastyh1233@gmail.com>
2023-04-14 20:16:19 +0300

    env

--- src/api/apiConfig/index.ts
+++ src/api/apiConfig/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: "https://social-network.samuraijs.com/api/1.0/",
+  baseURL: process.env.REACT_APP_BASE_URL,
   withCredentials: true,
-  headers: { "API-KEY": "78ba9efb-88a6-4c7f-b505-5ad3ba5a9466" },
+  headers: { "API-KEY": process.env.REACT_APP_API_KEY },
 });
